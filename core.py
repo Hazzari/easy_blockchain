633cffbf538fd3f049688fb4b4b490d0474d9143
Aleksan <Dev.aleksan@gmail.com>
2021-08-06 00:22:35 +0300

    feat: add base method blockchain

--- core.py
+++ core.py
@@ -1,22 +1,70 @@
+import hashlib
+import json
+from dataclasses import dataclass, field
+from time import time
+
+
+@dataclass
 class Blockchain:
-    def __init__(self):
-        self.chain = []
+    chain: list = field(default_factory=list)
+    current_transactions: list = field(default_factory=list)
+
+    def __post_init__(self):
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Создание нового блока в блокчейне
+
+        :param proof: <int> Доказательства проведенной работы
+        :param previous_hash: (Опционально) хеш предыдущего блока
+        :return: <dict> Новый блок
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Перезагрузка текущего списка транзакций
         self.current_transactions = []
 
-    def new_block(self):
-        # Создает новый блок и вносит его в цепь
-        pass
+        self.chain.append(block)
+        return block
 
-    def new_transaction(self):
-        # Вносит новую транзакцию в список транзакций
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Направляет новую транзакцию в следующий блок
 
-    @staticmethod
-    def hash(block):
-        # Хеширует блок
-        pass
+        :param sender: <str> Адрес отправителя
+        :param recipient: <str> Адрес получателя
+        :param amount: <int> Сумма
+        :return: <int> Индекс блока, который будет хранить эту транзакцию
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @property
     def last_block(self):
-        # Возвращает последний блок в цепочке
-        pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Создает хэш SHA-256 блока
+
+        :param block: <dict> Блок
+        :return: <str> Хеш блока
+        """
+
+        # Упорядочиваем хеш, иначе у нас будут противоречивые хеши
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
